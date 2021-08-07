eb2448856f093191efb166c9607547ea86ed9ede
Idan Shemy <ishemy@ucdavis.edu>
2021-08-07 23:32:16 +0300

    failed attempt, but added main function

--- p12.py
+++ p12.py
@@ -1,3 +1,22 @@
+from math import sqrt
+
+# naive solution
+def first_triangle_with_div(n):
+    m = 1
+
+    while (num_factors(m) < n):
+        m = triangle(m + 1)
+
+    return m
+
+# new def with integration
+def triangle(n):
+    if n <= 1:
+        return 1
+    else:
+        print(triangle(n-1) + n)
+        return triangle(n-1) + n
+
 
 def num_factors(n):
 
@@ -11,3 +30,6 @@ def num_factors(n):
     if n % sqrt(n) == 0: num += 1
 
     return num
+
+
+first_triangle_with_div(500)
