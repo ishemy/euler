238bf3a257ec34ce2a0a5ae8f4f67e493d677068
Idan Shemy <ishemy@ucdavis.edu>
2021-08-15 23:42:53 +0300

    implemented naive solution, too slow

--- p14.py
+++ p14.py
@@ -7,6 +7,16 @@ def collatz_chain(n):
         if n % 2 == 0: return 1 + collatz_chain(n/2)
         else: return 1 + collatz_chain(3*n + 1)
 
+# naive solution
+def longest_chain_under(n):
+    longest = -1
+
+    for i in range(n):
+        chain = collatz_chain(i)
+        if chain > longest: longest = chain
+
+    return longest
 
 
+print(longest_chain_under(1000000))
 
